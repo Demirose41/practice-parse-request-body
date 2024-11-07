6c27d1f9e6039c18ccccdf6969866e9414cf976d
demirose41 <demirose41@gmail.com>
2024-11-06 23:48:44 -0800

    completed practice

--- server.js
+++ server.js
@@ -5,7 +5,31 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here
+const http = require('http')
+
+server = http.createServer((req,res) => {
+    let reqBody = '';
+    req.on('data', (data) => {
+        reqBody += data
+    })
+
+    req.on('end', () =>{
+        reqBody = reqBody.split('&').map((ele) => ele.split('='));
+        reqBody = reqBody.map((pair) => pair.map((val) => decodeURIComponent(val)));
+        let obj = {}
+        console.log(reqBody)
+        reqBody.forEach((pair) => obj[pair[0]] = pair[1]);
+        console.log(obj) 
+    })
+    res.status = 200;
+    res.end();
+})
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log('Server is listening on port', port)
+});
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
